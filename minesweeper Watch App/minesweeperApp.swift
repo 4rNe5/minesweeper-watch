1c14991f5ab77e04ec938ae2ad8718d90c7064e5
Park Jun Hyeon / 4rNe5 <me@4rne5.dev>
2024-10-11 09:03:39 +0900

    Initial Commit

---
+++ minesweeper Watch App/minesweeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  minesweeperApp.swift
+//  minesweeper Watch App
+//
+//  Created by 4rNe5 on 10/11/24.
+//
+
+import SwiftUI
+
+@main
+struct minesweeper_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
